8e731a784ac69a420d6c0857c68fa26a603b930f
mabuodeh <mabuodeh@gmail.com>
2019-07-21 10:14:15 +0300

    Clean up

--- home/src/main/resources/static/assets/datatable/datatables-demo.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable(  {
-    "language": {
-
-      "url": "assets/datatable/arabic.json"
-    }
-  });
-});
