b32cafa4df5166fbd35bd4efeed78273d93e4046
mabuodeh <mabuodeh@gmail.com>
2019-08-20 08:29:16 +0300

    Combine Registry & Config Server

--- registry-server/src/main/java/jo/gov/ltrc/permit/registry/RegistryServerApplication.java
+++ registry-server/src/main/java/jo/gov/ltrc/permit/registry/RegistryServerApplication.java
@@ -2,10 +2,17 @@ package jo.gov.ltrc.permit.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
+@EnableEurekaClient
 public class RegistryServerApplication {
 
     public static void main(String[] args) {
